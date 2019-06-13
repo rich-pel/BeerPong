50e992bea2348c9814392c02deacdc38a6f7ede9
fradtke <fratke@uos.de>
2019-06-13 17:37:30 +0200

    I added the Ball Holder I updated the Cups I added some scripts for the basic game behavior

---
+++ Assets/Scripts/Controller/BallController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Maybe just if the collision enter the ballFallBeside
+        if (other.gameObject.tag.Equals("Ground") || other.gameObject.tag.Equals("Wall") ||
+            other.gameObject.tag.Equals("Table"))
+        {
+            GameManager.instance.BallFallBeside();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("CupHitArea"))
+        {
+            GameManager.instance.BallFallInCup();
+        }
+    }
+
+    public void WasTaken()
+    {
+        BallManager.instance.BallIsGrabbed();
+    }
+}
---
+++ Assets/Scripts/Manager/BallManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BallManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static BallManager instance;
+
+    public GameObject throwableBall;
+
+    public GameObject playersBallHolderArea;
+
+    public GameObject enemysBallHolderArea;
+
+    public int moveableTimeForBall = 10;
+
+    private float timeOutForBall;
+    private bool ballTimeIsTracked = false;
+    
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballTimeIsTracked && timeOutForBall < Time.time)
+        {
+            ballTimeIsTracked = !ballTimeIsTracked;
+            GameManager.instance.BallFallBeside();
+        }
+    }
+
+    public void SetPositionToBallHolder(bool enemysTurn)
+    {
+        if (enemysTurn)
+        {
+            throwableBall.transform.position = enemysBallHolderArea.transform.position;
+        }
+        else
+        {
+            throwableBall.transform.position = playersBallHolderArea.transform.position;
+        }
+    }
+
+
+    public void BallIsGrabbed()
+    {
+        Debug.Log("Ball is Grabbed");
+        ballTimeIsTracked = true;
+        timeOutForBall = Time.time + 10;
+    }
+
+    public float getCurrentTimeLeft()
+    {
+        if (ballTimeIsTracked)
+            return timeOutForBall - Time.time;
+        else
+            return moveableTimeForBall;
+    }
+}
