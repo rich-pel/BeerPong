6d5888ea0a94139edd60f9fd69ce7e48f1c2cb8f
Richard <rruppel@uni-osnabrueck.de>
2019-07-02 21:29:32 +0200

    Created new Player Prefab variant from SteamVR Player. Added PlayerController with Reset method and controller Input recognition. Added player spawn points.

---
+++ Assets/Editor/PlayerControllerEditor.cs
@@ -0,0 +1,22 @@
+﻿using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Unity;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerController))]
+public class PlayerControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PlayerController player = (PlayerController)target;
+        if (player != null)
+        {
+            if (GUILayout.Button("Reset VR position"))
+            {
+                player.ResetPosition();
+            }
+        }
+    }
+}
