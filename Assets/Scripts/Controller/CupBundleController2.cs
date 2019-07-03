907ae357b9f4a95e1d339ead6d63149e5f2071c5
Richard <rruppel@uni-osnabrueck.de>
2019-07-03 13:09:28 +0200

    Autospawn Cups by Script works!
    Todo: Clean up Code in CupController / CupBundleConroller / CupManager

--- Assets/Scripts/Controller/CupBundleController2.cs
+++ Assets/Scripts/Controller/CupBundleController2.cs
@@ -3,55 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 
-/*           0 1 2 3 4 5 6   
- *       0         +
- *       1       +   +
- *       2     +   +   +
- *       3   +   +   +   +
+/*       y
+ *     x    -3 -2 -1 0  1  2  3
+ *       0           +
+ *       1         +    +
+ *       2      +    +     +
+ *       3   +     +    +     +
  */
-
-
-
 
 
 public class CupBundleController2 : MonoBehaviour
 {
     [SerializeField] private GameObject cupPrefab;
+    [SerializeField] private float radius = 0.7f;
 
     // TODO: Get from GameManager?
     // #maxCups
 
-    private int numRows;
+    private int _numRows = 4;
+    private float _gab = 0.011f;
 
-    private float radius =.1f;
+    private float _distanceX;
+    private float _distanceY;
 
     // Start is called before the first frame update
     void Start()
     {
+        _distanceY = radius + _gab;
+        _distanceX = radius / 2 + _gab;
+
+        // get radius from somewhere
+
         CreateCups();
     }
 
-
-
-
+    /// <summary>
+    /// Instantiates Cups in diamond formation
+    /// </summary>
     void CreateCups()
     {
-        numRows = 4;
-        float row = 0;
 
-        for (int y = 0; y < numRows; y++)
+        int _count=0;
+
+
+
+        for (int y = 0; y < _numRows; y++)
         {
             for (int x = -y; x <= y; x+=2)
             {
-                Debug.Log("x: " +x + " y: " + y);
+                // local position
+                // Vector3 pos = new Vector3(x * _distance /2, 0f, y * _distance);
+                Vector3 pos = new Vector3(x * _distanceX , 0f, y * _distanceY);
 
-                Vector3 pos =  new Vector3( x * radius, 0f, y * radius);
-                GameObject gamaObject = Instantiate(cupPrefab, transform.position + pos , Quaternion.identity, transform) as GameObject;
+                // Instance settings
+                GameObject myGO = Instantiate(cupPrefab, transform);
+                myGO.name = "Cup" + _count;
+                myGO.transform.localPosition = pos;
+
+                _count++;
+                // TODO: Lass das den copController machen: myGO.GetComponent<CupController>().father = this.transform; 
             }
         }
 
     }
 
 }
-//Instantiate(cupPrefab, new Vector3(-i * radius, 0f, radius), Quaternion.identity, transform);
+//GameObject gamaObject = Instantiate(cupPrefab, transform.position + pos , Quaternion.identity, transform) as GameObject;
 
