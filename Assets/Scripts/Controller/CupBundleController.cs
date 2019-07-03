5ce25dc74dfd7b618d21e701d5e38a3f193e49f0
Richard <rruppel@uos.de>
2019-07-03 18:41:31 +0200

    merge compelted bug fixes

--- Assets/Scripts/Controller/CupBundleController.cs
+++ Assets/Scripts/Controller/CupBundleController.cs
@@ -1,17 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Unity;
+using BeardedManStudios.Forge.Networking.Generated;
 using UnityEngine;
 
 public class CupBundleController : MonoBehaviour
 {
-    [SerializeField] private List<CupController> cupsInGroup;
+    private List<CupController> cupsInGroup = new List<CupController>();
+
+    [SerializeField] private int NetworkPrefabIndex;
+    [SerializeField] private float radius = 0.7f;
+
+    private int _numRows = 4;
+    private float _gab = 0.011f;
+
+    private float _distanceX;
+    private float _distanceY;
+
+
 
     void Start()
     {
-        foreach (CupController cup in cupsInGroup)
+        // get radius from somewhere
+        _distanceY = radius + _gab;
+        _distanceX = radius / 2 + _gab;
+
+        if (NetworkPrefabIndex < 0 || NetworkPrefabIndex >= NetworkManager.Instance.SyncedCupNetworkObject.Length)
         {
-            cup.father = this;
+            Debug.LogError("NetworkPrefabIndex is out of Range!");
+            return;
         }
+
+        CreateCups();
+    }
+
+    /// <summary>
+    /// Instantiates Cups in diamond formation
+    /// </summary>
+    void CreateCups()
+    {
+
+        int _count = 0;
+
+        for (int y = 0; y < _numRows; y++)
+        {
+            for (int x = -y; x <= y; x += 2)
+            {
+                GameObject prefab = NetworkManager.Instance.SyncedCupNetworkObject[NetworkPrefabIndex];
+
+                // local position
+                // Vector3 pos = new Vector3(x * _distance /2, 0f, y * _distance);
+                Vector3 pos = new Vector3(x * _distanceX, 0.0f, y * _distanceY);
+
+                Quaternion rot = prefab.transform.rotation;
+                pos = transform.TransformPoint(pos); // dis is fucking neat man!
+
+                // Instance settings
+                SyncedCupBehavior syncedCup = NetworkManager.Instance.InstantiateSyncedCup(NetworkPrefabIndex, pos, rot, true);
+                CupController cup = syncedCup as CupController;
+
+                // this should actually never fail
+                if (cup == null)
+                {
+                    Debug.LogError("SyncedCupBehavior is not a CupController!");
+                    return;
+                }
+
+                GameObject myGO = syncedCup.gameObject;
+
+                
+                myGO.name = "Cup" + _count;
+                myGO.transform.localPosition = pos;
+                cup.father = this;
+                cupsInGroup.Add(cup);
+
+                _count++;
+     
+                
+                // TODO: Lass das den copController machen: myGO.GetComponent<CupController>().father = this.transform; 
+            }
+        }
+
     }
 
     public int GetNumberOfActiveCups()
